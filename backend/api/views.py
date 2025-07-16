25367f6e594f5a7e1f1a5c09d820602774dd155e
Tharushi Umesha Mahipala <145181808+Tharushi-Umesha@users.noreply.github.com>
2025-07-16 20:18:38 +0530

    initial release of django react tutorial

---
+++ backend/api/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render;
+from django.http import HttpResponse;
+from rest_framework import viewsets, permissions;
+from .serializers import * ;
+from .models import *;
+from rest_framework.response import Response;
+
+
+def home(request):
+    return HttpResponse("This is the home page.")
+
+class ProjectViewset(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def list(self, request):
+        queryset = self.queryset
+        serializer = self.serializer_class(queryset, many = True)
+        return Response(serializer.data)
+        
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status= 400)
+    # def create(self, request):
+    #     serializer = self.serializer_class(data=request.data)
+    #     if serializer.is_valid():  # ✅ Call the method
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     else:
+    #         return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, pk=None):
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status= 400)
+
+    def destroy(self, request, pk=None):
+        project = self.queryset.get(pk=pk)
+        project.delete()
+        return Response(status=204)
